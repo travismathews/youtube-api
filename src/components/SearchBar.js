0c25477b1f37ef48adc462ada2b121df7bf84fe2
Travis Mathews <xenoaurora@gmail.com>
2021-10-27 23:08:04 -0400

    Controlled Search Component attached to state

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    
+    // TODO: make sure we call the callback from parent component 
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
