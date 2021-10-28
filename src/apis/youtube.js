285b110ff79e26756a85ab223421b3fc569efaf4
Travis Mathews <xenoaurora@gmail.com>
2021-10-27 23:50:15 -0400

    Working youtube api search and saves returned array to state

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const KEY = "AIzaSyB4TP-VWrEGj74MmZN9xpDNcjvXo8tvRPU";
 
 export default axios.create({
-  baseURL: "https://www.googleapis.com/discovery/v1/apis/youtube/v3",
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
+    type: 'video',
     key: KEY,
   },
 });
